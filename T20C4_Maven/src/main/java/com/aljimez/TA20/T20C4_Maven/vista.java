9cd442ec3c8c5f98cf1303a612c07310b4445a70
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-04-10 22:59:00 +0200

    Modificacion de etiqueta

--- T20C4_Maven/src/main/java/com/aljimez/TA20/T20C4_Maven/vista.java
+++ T20C4_Maven/src/main/java/com/aljimez/TA20/T20C4_Maven/vista.java
@@ -6,10 +6,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 
 public class vista extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Create the frame.
@@ -23,8 +29,44 @@ public class vista extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+		
 		JLabel lblNewLabel = new JLabel("Etiqueta");
-		lblNewLabel.setBounds(108, 41, 46, 14);
+		lblNewLabel.setBounds(105, 11, 46, 14);
 		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String text = textField.getText();
+				int size = Integer.parseInt(text);
+				lblNewLabel.setBounds(105, 11, size, 14);
+
+			}
+		});
+		
+		textField.setBounds(68, 102, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String text = textField.getText();
+				int size = Integer.parseInt(text);
+				lblNewLabel.setBounds(105, 11, size, size);
+			}
+		});
+		textField_1.setBounds(68, 148, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("width");
+		lblNewLabel_1.setBounds(183, 105, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("height");
+		lblNewLabel_1_1.setBounds(183, 151, 46, 14);
+		contentPane.add(lblNewLabel_1_1);
 	}
 }
